84dad10be9dbebc7d1e2bac4493ea2d5b3ff4ade
Christian Vogel <chris.vogel@me.com>
2018-10-05 13:57:50 +0200

    #9 margin-left on container not working

--- src/plugins/process-layout-style.ts
+++ src/plugins/process-layout-style.ts
@@ -108,84 +108,74 @@ export function transformWidget(widget: Widget, options: Options): Widget {
 		})
 	}
 
+
 	// padding
 
-	if(paddingParam && paddingParam.value && paddingParam.type == 'expression') {
+	let paddings: { top?: string, right?: string, bottom?: string, left?: string } = {}
+	if (paddingParam && paddingParam.value) {
+		paddings = parseTRBLStyle(paddingParam.value.toString())
+	}
+	if (paddingTopParam && paddingTopParam.value) {
+		paddings.top = paddingTopParam.value.toString()
+	}
+	if (paddingRightParam && paddingRightParam.value) {
+		paddings.right = paddingRightParam.value.toString()
+	}
+	if (paddingBottomParam && paddingBottomParam.value) {
+		paddings.bottom = paddingBottomParam.value.toString()
+	}
+	if (paddingLeftParam && paddingLeftParam.value) {
+		paddings.left = paddingLeftParam.value.toString()
+	}
+	if (Object.keys(paddings).length > 0) {
+		const params: string[] = []
+		if (paddings.top) params.push('top: ' + parseStyleDoubleValue(paddings.top))
+		if (paddings.right) params.push('right: ' + parseStyleDoubleValue(paddings.right))
+		if (paddings.bottom) params.push('bottom: ' + parseStyleDoubleValue(paddings.bottom))
+		if (paddings.left) params.push('left: ' + parseStyleDoubleValue(paddings.left))
+		const inset = `EdgeInsets.only(${params.join(', ')})`
 		widget.params.push({
 			class: 'param',
 			name: 'padding',
 			type: 'expression',
-			value: paddingParam.value.toString(),
+			value: inset,
 			resolved: true
 		})
-	} else {
-		let paddings: { top?: string, right?: string, bottom?: string, left?: string } = {}
-		if (paddingParam && paddingParam.value) {
-			paddings = parseTRBLStyle(paddingParam.value.toString())
-		}
-		if (paddingTopParam && paddingTopParam.value) {
-			paddings.top = paddingTopParam.value.toString()
-		}
-		if (paddingRightParam && paddingRightParam.value) {
-			paddings.right = paddingRightParam.value.toString()
-		}
-		if (paddingBottomParam && paddingBottomParam.value) {
-			paddings.bottom = paddingBottomParam.value.toString()
-		}
-		if (paddingLeftParam && paddingLeftParam.value) {
-			paddings.left = paddingLeftParam.value.toString()
-		}
-		if (Object.keys(paddings).length > 0) {
-			const params: string[] = []
-			if (paddings.top) params.push('top: ' + parseStyleDoubleValue(paddings.top))
-			if (paddings.right) params.push('right: ' + parseStyleDoubleValue(paddings.right))
-			if (paddings.bottom) params.push('bottom: ' + parseStyleDoubleValue(paddings.bottom))
-			if (paddings.left) params.push('left: ' + parseStyleDoubleValue(paddings.left))
-			const inset = `EdgeInsets.only(${params.join(', ')})`
-			widget.params.push({
-				class: 'param',
-				name: 'padding',
-				type: 'expression',
-				value: inset,
-				resolved: true
-			})
-		}
 	}
 
 
 	// margin
-	if(marginParam && marginParam.value) {
-		let margins: { top?: string, right?: string, bottom?: string, left?: string } = {}
-		if (marginParam && marginParam.value) {
-			margins = parseTRBLStyle(marginParam.value.toString())
-		}
-		if (marginTopParam && marginTopParam.value) {
-			margins.top = marginTopParam.value.toString()
-		}
-		if (marginRightParam && marginRightParam.value) {
-			margins.right = marginRightParam.value.toString()
-		}
-		if (marginBottomParam && marginBottomParam.value) {
-			margins.bottom = marginBottomParam.value.toString()
-		}
-		if (marginLeftParam && marginLeftParam.value) {
-			margins.left = marginLeftParam.value.toString()
-		}
-		if (Object.keys(margins).length > 0) {
-			const params: string[] = []
-			if (margins.top) params.push('top: ' + parseStyleDoubleValue(margins.top))
-			if (margins.right) params.push('right: ' + parseStyleDoubleValue(margins.right))
-			if (margins.bottom) params.push('bottom: ' + parseStyleDoubleValue(margins.bottom))
-			if (margins.left) params.push('left: ' + parseStyleDoubleValue(margins.left))
-			const inset = `EdgeInsets.only(${params.join(', ')})`
-			widget.params.push({
-				class: 'param',
-				name: 'margin',
-				type: 'expression',
-				value: inset,
-				resolved: true
-			})
-		}
+	
+	let margins: { top?: string, right?: string, bottom?: string, left?: string } = {}
+	if (marginParam && marginParam.value) {
+		margins = parseTRBLStyle(marginParam.value.toString())
+	}
+	if (marginTopParam && marginTopParam.value) {
+		margins.top = marginTopParam.value.toString()
+	}
+	if (marginRightParam && marginRightParam.value) {
+		margins.right = marginRightParam.value.toString()
+	}
+	if (marginBottomParam && marginBottomParam.value) {
+		margins.bottom = marginBottomParam.value.toString()
+	}
+	if (marginLeftParam && marginLeftParam.value) {
+		margins.left = marginLeftParam.value.toString()
+	}
+	if (Object.keys(margins).length > 0) {
+		const params: string[] = []
+		if (margins.top) params.push('top: ' + parseStyleDoubleValue(margins.top))
+		if (margins.right) params.push('right: ' + parseStyleDoubleValue(margins.right))
+		if (margins.bottom) params.push('bottom: ' + parseStyleDoubleValue(margins.bottom))
+		if (margins.left) params.push('left: ' + parseStyleDoubleValue(margins.left))
+		const inset = `EdgeInsets.only(${params.join(', ')})`
+		widget.params.push({
+			class: 'param',
+			name: 'margin',
+			type: 'expression',
+			value: inset,
+			resolved: true
+		})
 	}
 
 
