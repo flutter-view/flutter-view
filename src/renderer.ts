a748ff7926f23237380c599e624406892fad4c30
Christian Vogel <chris.vogel@me.com>
2018-10-15 16:24:35 +0200

    array tags now always render as __flatten(array), allowing nested lists in arrays

--- src/renderer.ts
+++ src/renderer.ts
@@ -318,9 +318,9 @@ export function renderDartFile(dartFile: string, widgets: Widget[], imports: str
 				const widgets = param.value as Widget[]
 				const values = widgets.map(widget=>`${renderWidget(widget, options)}`)
 				return multiline(
-					`[`,
+					`__flatten([`,
 					indent(values.join(',\n'), options.indentation),
-					`]`
+					`])`
 				)
 			}
 			case 'widgets': {
