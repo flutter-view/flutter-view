2c3aeb941c09bd2a742bef170eb5e921df55ab46
Christian Vogel <chris.vogel@me.com>
2018-10-22 13:26:09 +0200

    added display css property. display: none will not create any dart for that tag and children.

---
+++ src/plugins/process-display-params.ts
@@ -0,0 +1,60 @@
+import { pull } from 'lodash';
+import { Widget } from '../models/flutter-model';
+import { applyOnDescendants, findAndRemoveParam, findParam } from '../tools';
+import { Options } from '../watcher';
+
+/**
+ * This plugin processes the **as** parameters.
+ * 
+ * It finds all child/children widgets with the as="..." parameter, 
+ * and changes any of those widgets from child(ren) to direct parameters of
+ * the name in the as parameter value.
+ * 
+ * @param widget the widget tree to process
+ * @param options the flutter-view options
+ * @return the transformed widget tree
+ */
+export function transformWidget(widget: Widget, options: Options): Widget {
+
+	const childrenParam = findParam(widget, 'children', true)
+	if(childrenParam) {
+		const params: { name: string, value: Widget }[] = []
+		const children = childrenParam.value as Widget[]
+		// we cannot remove while iterating the children, so we do this in two steps:
+		// 1) find all the children to be removed
+		for(let child of children) {
+			const displayParam = findAndRemoveParam(child, 'display', {
+				includeResolved: true,
+				includeExpressions: true
+			})
+			if(displayParam) console.log('DISPLAY ' + displayParam)
+			if(displayParam && displayParam.value == 'none') {
+				params.push({ 
+					name: displayParam.value.toString(), 
+					value: child 
+				})
+			}
+		}
+		// 2) remove each param's widget from the children param widget
+		for(let param of params) {
+			pull(children, param.value)
+		}
+	} 
+
+	const childParam = findParam(widget, 'child', true)
+	if(childParam) {
+		const child = childParam.value as Widget
+		const displayParam = findAndRemoveParam(child, 'display')
+		if(displayParam) console.log('DISPLAY ' + displayParam)
+		if(displayParam && displayParam.value == 'none') {
+			findAndRemoveParam(widget, 'child', {
+				includeResolved: true,
+				includeExpressions: true
+			})
+		}
+	}
+
+	applyOnDescendants(widget, descendant=>transformWidget(descendant, options))
+
+	return widget	
+}
--- src/watcher.ts
+++ src/watcher.ts
@@ -59,6 +59,7 @@ const defaultOptions: Options = {
 	indentation: 2,
 	plugins: [
 		"./plugins/styles-to-params",
+		"./plugins/process-display-params",
 		"./plugins/process-theme-style",
 		"./plugins/process-duration-params",
 		"./plugins/process-scoped-animation-tag",
